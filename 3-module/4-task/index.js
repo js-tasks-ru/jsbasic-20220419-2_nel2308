855d8bb7876ab42ac180ab1baefae36f2afb6b75
nel2308 <nel2308@mail.ru>
2022-05-06 15:36:19 +0300

    всё сделано

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,1 +1,7 @@
-
+function showSalary(users, age) {
+  return users.filter(user => {
+    return user.age <= age;
+  }).map(user => {
+    return user.name + ', ' + user.balance;
+  }).join("\n");
+}
