f6db71b89085e3b04a0c5a97ce844357df110ea1
Нелли Никитина <nel2308@mail.ru>
2022-05-13 18:50:47 +0300

    доделано

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
-}
+  let ul = document.createElement('ul');
+      for (elem in friends){
+          let li = document.createElement('li');
+          let a = document.createElement('a');
+          $(a).appendTo(li);
+          $(a).attr('href',friends[elem]);
+          $(a).text(elem);
+          $(li).appendTo(ul);
+      }
+  $(ul).appendTo('body');
+  };
